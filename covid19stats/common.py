1630d1b4be590d38f24807d5ee4f5ec87ad707f5
codeforkjeff <jeff@codefork.com>
2020-07-14 13:34:42 -0700

    factor out download_and_update

--- covid19stats/common.py
+++ covid19stats/common.py
@@ -3,9 +3,13 @@
 
 from collections import namedtuple
 import functools
+import hashlib
+import os
+import os.path
 import pathlib
 import sqlite3
 import time
+import urllib.request
 
 
 Path = namedtuple('Path', ['path', 'date'])
@@ -39,3 +43,53 @@ def get_db_conn():
 
 def touch_file(path):
     pathlib.Path(path).touch()
+
+
+# 3 hours
+UPDATE_THRESHOLD = 60 * 60 * 3
+
+
+def download_and_update(url, path, threshold=UPDATE_THRESHOLD):
+
+    hash = None
+    size = None
+    exists = os.path.exists(path)
+
+    path_latest = "/tmp/download_latest_" + str(time.time())
+    hash_latest = None
+    size_latest = None
+
+    downloaded = False
+
+    if (not exists) or time.time() - os.path.getmtime(path) > threshold:
+        with urllib.request.urlopen(url) as f:
+            data = f.read()
+            hash_latest = hashlib.md5(data)
+            with open(path_latest, 'wb') as output:
+                output.write(data)
+            size_latest = os.path.getsize(path_latest)
+        downloaded = True
+
+    replace = False
+
+    if downloaded:
+        if exists:
+            size = os.path.getsize(path)
+            if size == size_latest:
+                with open(path, 'rb') as f:
+                    data = f.read()
+                    hash = hashlib.md5(data)
+                if hash != hash_latest:
+                    replace = True
+            else:
+                replace = True
+        else:
+            replace = True
+
+    if downloaded:
+        if replace:
+            print("Downloaded file is different, replacing file")
+            os.rename(path_latest, path)
+        else:
+            os.remove(path_latest)
+
--- covid19stats/covidtracking_download.py
+++ covid19stats/covidtracking_download.py
@@ -1,42 +1,12 @@
 
-import hashlib
-import os
-import os.path
-import time
-import urllib.request
+from .common import download_and_update
 
 
 def update_covidtracking_states():
 
-    path = "input/covidtracking_states.csv"
-    hash = None
-
-    path_latest = "input/covidtracking_states_latest.csv"
-    hash_latest = None
-
-    # seconds
-    THRESHOLD = 60 * 60 * 3
-
-    if os.path.exists(path):
-        with open(path, 'rb') as f:
-            data = f.read()
-            hash = hashlib.md5(data)
-
-    if (not os.path.exists(path)) or time.time() - os.path.getmtime(path) > THRESHOLD:
-        with urllib.request.urlopen("https://covidtracking.com/api/v1/states/daily.csv") as f:
-            data = f.read()
-            hash_latest = hashlib.md5(data)
-            with open(path_latest, 'wb') as output:
-                output.write(data)
-
-    if hash_latest:
-        if hash != hash_latest:
-            print("Replacing covid states file")
-            os.rename(path_latest, path)
-        else:
-            os.remove(path_latest)
+    download_and_update("https://covidtracking.com/api/v1/states/daily.csv", "input/covidtracking_states.csv")
 
 
 if __name__ == "__main__":
-    update_covidtracking_states()
 
+    update_covidtracking_states()
