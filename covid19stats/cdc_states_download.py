cd3acfc1d599d87d41ef83b5cb044c56cf38d836
codeforkjeff <jeff@codefork.com>
2021-07-08 22:32:47 -0700

    change threshold to 12h

--- covid19stats/cdc_states_download.py
+++ covid19stats/cdc_states_download.py
@@ -4,8 +4,8 @@ from .common import timer, download_and_update
 @timer
 def download_cdc_states_data():
 
-    # every day
-    threshold = 60 * 60 * 24 * 1
+    # every 12h
+    threshold = 60 * 60 * 12
 
     download_and_update( \
         'https://data.cdc.gov/api/views/9mfq-cb36/rows.tsv?accessType=DOWNLOAD&bom=true', \
