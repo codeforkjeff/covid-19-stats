d0216a547917c90c0bd405fafbcca098872e4fc4
Jeff <jeff@codefork.com>
2021-12-23 21:51:09 -0800

    put .meltano dir in a docker volume; small tweaks

--- meltano_docker.sh
+++ meltano_docker.sh
@@ -3,8 +3,11 @@
 # docker pull meltano/meltano:v1.90.1
 
 docker run \
+    --rm \
     -v $(pwd):/project \
-    -v "$HOME/bigquery/service-account.json":"/service-account.json" \
+    -v covid-19-stats-meltano-data:/project/.meltano \
+    -v covid-19-stats-stage:"/project/stage" \
+    -v "$(pwd)/service-account.json":"/service-account.json" \
     -e "GOOGLE_APPLICATION_CREDENTIALS=/service-account.json" \
     -w /project \
     meltano/meltano:v1.90.1 \
