20d333a775e29f15fc19524545147ac136824cd0
codeforkjeff <jeff@codefork.com>
2022-06-12 23:51:25 -0700

    more wip

--- main.sh
+++ main.sh
@@ -8,33 +8,17 @@ source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
 
 echo "==== Running at `date`"
 
-if [ -z "$PROJECT_DIR" ]; then
-    PROJECT_DIR=~
-fi
+script_dir="$(dirname $0)"
 
 echo Using project directory: $PROJECT_DIR
 
 ## clone or update
 
-cd $PROJECT_DIR
-
-if [ ! -d "covid-19-stats" ]; then
-    git clone git@github.com:codeforkjeff/covid-19-stats.git
-fi
-
-## clone or update
-
-cd $PROJECT_DIR
+cd $script_dir/data
 
 if [ ! -d "COVID-19" ]; then
     git clone git@github.com:CSSEGISandData/COVID-19.git
 fi
-
-## make sure repo is up to date
-
-cd $PROJECT_DIR/covid-19-stats
-git checkout master
-git pull
 
 # setup environments / make sure they're up to date
 
