52ea31793a2a7dd2249d1dcd2482296a590a25b1
Jeff Chiu <jeff@codefork.com>
2022-06-27 22:36:27 -0700

    exit with non-0 status when pip install fails

--- create_envs.sh
+++ create_envs.sh
@@ -6,12 +6,12 @@ mkvirtualenv covid-19-stats-load
 
 python3 -m pip install --upgrade setuptools
 python3 -m pip install --upgrade pip
-pip install -r requirements-load.txt
+pip install -r requirements-load.txt || exit 1
 
 mkvirtualenv covid-19-stats-dbt
 
 python3 -m pip install --upgrade setuptools
 python3 -m pip install --upgrade pip
-pip install -r requirements-dbt.txt
+pip install -r requirements-dbt.txt || exit 1
 
 python3 -m pip cache purge
